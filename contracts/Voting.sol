3dec39af46701b3016422cd03d25be857b7fd79e
aniketcodes <aniketsinha1304@gmail.com>
2022-01-13 15:56:30 +0530

    variables in ballot contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,2 +1,30 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 < 0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Ballot {
+    //variables
+    struct vote {
+        address voterAddress;
+        bool choice;
+    }
+
+    struct voter {
+        string voterName;
+        bool voted;
+    }
+    
+    uint private countResult=0;
+    uint public finalResult =0;
+    uint public totalVoter=0;
+    uint public totalVote=0;
+
+    address public ballotOfficialAddress;
+    string public ballotOfficialName;
+    string public proposal;
+
+    mapping(uint=>vote) private votes;
+    mapping(address => voter) public voterRegister;
+
+    enum State{Created,Voting,Ended}
+    State public state;
+}
